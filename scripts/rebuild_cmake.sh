39271bb260fbd2fefc78fb1b22b67bbdc5c1a384
Frank Meier <frank.meier@tu-dortmund.de>
2014-11-06 11:01:13 +0100

    restructured git repository concerning cmake and ignorelist

---
+++ scripts/rebuild_cmake.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CMAKE_BASE_DIR="$(dirname "$DIR")"
+
+if [ -f CMakeLists.txt ]
+then 
+  echo "Directory contains CMakeLists.txt. You should use a clean out of source build. Exiting."
+  exit 1
+fi
+
+if [ -f CMakeCache.txt ]
+then 
+  echo "Directory contains CMake files, will clean up."
+  rm -rf bin/ CMake* cmake_install.cmake Makefile lib/ main/ src/
+fi
+
+echo "Building CMake build system."
+if [ "$1" = "debug" ]
+then
+  cmake -DCMAKE_BUILD_TYPE=Debug $CMAKE_BASE_DIR
+elif [ "$1" = "profiling" ]
+then
+  cmake -DCMAKE_BUILD_TYPE=Profiling $CMAKE_BASE_DIR
+else 
+  cmake -DCMAKE_BUILD_TYPE=Release $CMAKE_BASE_DIR
+fi
+
+echo "creating symbolic links"
+ln -s $CMAKE_BASE_DIR/config config
+ln -s $CMAKE_BASE_DIR/python python 
