5daad7f11722e1e1fd83aaded714dc87c85c8e54
Frank Meier <frank.meier@tu-dortmund.de>
2015-03-27 17:13:41 +0100

    new configs for training BDTs and writing BDT output into tuples

--- config/RunScripts/Scythe_B02DD_2012.sh
+++ config/RunScripts/Scythe_B02DD_2012.sh
@@ -2,9 +2,9 @@
 
 RUN_NAME=Production
 INPUT_PATH=/fhgfs/groups/e5/lhcb/NTuples/B02DD/Data/2012/
-INPUT_FILE_NAME=DT2012_B02DD_Stripping20r0_DVv35r1_20141102_fmeier_TupleA.root
-BASE_FILE_NAME=DT2012_B02DD_Stripping20r0_DVv35r1_20141102_fmeier
-DATE=20141120
+INPUT_FILE_NAME=DT2012_B02DD_Stripping21_DVv36r1_20150322_fmeier_combined_TupleA.root
+BASE_FILE_NAME=DT2012_B02DD_Stripping21_DVv36r1_20150322_fmeier_combined
+DATE=20150323
 PRODUCER=fmeier
 
 echo "Starting Scythe ${RUN_NAME}..."
@@ -12,46 +12,37 @@ echo "Starting Scythe ${RUN_NAME}..."
 echo "using input file: ${INPUT_PATH}${INPUT_FILE_NAME}"
 echo "starting reducers..."
 
-#echo ""
-#echo "**TupleA** (flat)"
-#echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_TupleA.root"
-#ArrayFlattenerGrimReaper "${INPUT_PATH}${INPUT_FILE_NAME}" B02DD/DecayTree "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_TupleA.root" B02DD /home/fmeier/git/b02dd/config/Reducer/FlattenTuple.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat.log
-
-#echo ""
-#echo "**TupleAPlus** (flat, common variables)"
-#echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_TupleAPlus.root"
-#B02DDVariablesGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_TupleA.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_TupleAPlus.root" B02DD > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars.log
-
-#echo ""
-#echo "**Tuple B** (flat, common variables, pre-selection)"
-#echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_TupleB.root"
-#MultiCutGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_TupleAPlus.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_noDcuts_TupleB.root" B02DD /home/fmeier/git/b02dd/config/Reducer/PreSelection.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars_preselected.log
+echo ""
+echo "**TupleAPlus** (common variables)"
+echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleAPlus.root"
+B02DDVariablesGrimReaper "${INPUT_PATH}${INPUT_FILE_NAME}" B02DD/DecayTree "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleAPlus.root" B02DD > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars.log
 
 echo ""
-echo "**Tuple B** (flat, common variables, pre-selected, BDT selection of Dplus and Dminus)"
-echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_BDT_TupleB.root"
-TMVAGrimReaper /home/fmeier/git/b02dd/config/Reducer/TMVAGrimReaper_Dplus.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars_preselected_cuts_bdtDplus.log
-TMVAGrimReaper /home/fmeier/git/b02dd/config/Reducer/TMVAGrimReaper_Dminus.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars_preselected_cuts_bdtDminus.log
-#TMVAGrimReaper /home/fmeier/git/b02dd/config/Reducer/TMVAGrimReaper_DD.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars_preselected_bdtDD.log
-#
-#echo ""
-#echo "**Tuple B** (flat, common variables, pre-selection, BDT selection of Dplus and Dminus, final selection)"
-#echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_TupleB.root"
-#MultiCutGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_BDT_TupleB.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_TupleB.root" B02DD /home/fmeier/git/b02dd/config/Reducer/FinalSelection.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars_preselected_finalselection.log
-#
-#echo ""
-#echo "**Tuple B** (flat, common variables, pre-selection, BDT selection of Dplus and Dminus, final selection, FT variables)"
-#echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_FT_TupleB.root"
-#FlavourTaggingGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_TupleB.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_FT_TupleB.root" B02DD B02DD > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars_preselected_finalselection_ft.log
-#
-#echo ""
-#echo "**Tuple B** (flat, common variables, pre-selection, BDT selection of Dplus and Dminus, final selection, FT variables, random candidate selection)"
-#echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_random_TupleB.root"
-#CandidateSelectionGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_FT_TupleB.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_random_TupleB.root" B02DD "idxRandom" > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars_preselected_finalselection_ft_random.log
-#
-#echo ""
-#echo "**Fit Tuple** (flat, common variables, pre-selection, BDT selection of Dplus and Dminus, final selection, FT variables, random candidate selection)"
-#echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_random_FitTuple.root"
-#FitTupleGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_random_TupleB.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_flat_selected_random_FitTuple.root" B02DD > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_flat_vars_preselected_finalselection_ft_random_fittuple.log
+echo "**Tuple B** (common variables, pre-selection)"
+echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleB.root"
+MultiCutGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleAPlus.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleB.root" B02DD /home/fmeier/git/b02dd/config/Reducer/PreSelection.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected.log
+
+echo ""
+echo "**Tuple B** (common variables, pre-selected, BDT selection)"
+echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_BDT_TupleB.root"
+TMVAGrimReaper /home/fmeier/git/b02dd/config/Reducer/TMVAGrimReaper_BDT1.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_bdt1.log
+TMVAGrimReaper /home/fmeier/git/b02dd/config/Reducer/TMVAGrimReaper_BDT2.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_bdt2.log
+TMVAGrimReaper /home/fmeier/git/b02dd/config/Reducer/TMVAGrimReaper_BDT3.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_bdt3.log
+TMVAGrimReaper /home/fmeier/git/b02dd/config/Reducer/TMVAGrimReaper_BDT4.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_bdt4.log
+
+# echo ""
+# echo "**Tuple B** (common variables, pre-selection, BDT selection, final selection)"
+# echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_selected_TupleB.root"
+# MultiCutGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_BDT_TupleB.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_selected_TupleB.root" B02DD /home/fmeier/git/b02dd/config/Reducer/FinalSelection.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_bdt_finalselection.log
+
+# echo ""
+# echo "**Tuple B** (common variables, pre-selection, BDT selection, final selection, FT variables)"
+# echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_selected_FT_TupleB.root"
+# FlavourTaggingGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_selected_TupleB.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_selected_FT_TupleB.root" B02DD B02DD > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_bdt_finalselection_ft.log
+
+# echo ""
+# echo "**Fit Tuple** (common variables, pre-selection, BDT selection, final selection, FT variables)"
+# echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_selected_FitTuple.root"
+# FitTupleGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_selected_FT_TupleB.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_selected_FitTuple.root" B02DD > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_bdt_finalselection_ft_fittuple.log
 
 echo "finished!"
