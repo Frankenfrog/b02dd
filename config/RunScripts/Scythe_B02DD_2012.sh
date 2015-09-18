691c3874d111d0cdb417abddcb5d7b65af3301d3
Frank Meier <frank.meier@tu-dortmund.de>
2015-09-18 12:54:35 +0200

    adapted run scripts for selection chain to kkpikpipi final state tuples

--- config/RunScripts/Scythe_B02DD_2012.sh
+++ config/RunScripts/Scythe_B02DD_2012.sh
@@ -2,9 +2,9 @@
 
 RUN_NAME=Production
 INPUT_PATH=/fhgfs/groups/e5/lhcb/NTuples/B02DD/Data/2012/
-INPUT_FILE_NAME=DT2012_B02DD_Stripping21_DVv36r5_20150908_pseyfert_combined_TupleA_kpipikpipi.root
-BASE_FILE_NAME=DT2012_B02DD_Stripping21_DVv36r5_20150908_pseyfert_combined_
-DATE=20150911
+INPUT_FILE_NAME=DT2012_B02DD_Stripping21_DVv36r5_20150909_pseyfert_combined_TupleA_kkpikpipi_reduced.root
+BASE_FILE_NAME=DT2012_B02DD_Stripping21_DVv36r5_20150909_pseyfert_combined_kkpikpipi
+DATE=20150917
 PRODUCER=fmeier
 
 echo "Starting Scythe ${RUN_NAME}..."
@@ -15,7 +15,7 @@ echo "starting reducers..."
 echo ""
 echo "**TupleAPlus** (common variables)"
 echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleAPlus.root"
-B02DDVariablesGrimReaper "${INPUT_PATH}${INPUT_FILE_NAME}" B02DD/DecayTree "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleAPlus.root" B02DD > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars.log
+B02DDVariablesGrimReaper "${INPUT_PATH}${INPUT_FILE_NAME}" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleAPlus.root" B02DD > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars.log
 
 echo ""
 echo "**Tuple B** (common variables, pre-selection)"
@@ -28,7 +28,7 @@ echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleC.root"
 MultiCutGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleB.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleC.root" B02DD /home/fmeier/git/b02dd/config/Reducer/MassVetos.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_massvetos.log
 
 echo ""
-echo "**Tuple C** (common variables, pre-selection, vetos + PID cuts, BDT selection)"
+echo "**Tuple CPlus** (common variables, pre-selection, vetos + PID cuts, BDT selection)"
 echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleCPlus.root"
 TMVAGrimReaper /home/fmeier/git/b02dd/config/Reducer/TMVAGrimReaper_2012.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_massvetos_bdt.log
 
@@ -38,7 +38,7 @@ echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleD.root"
 MultiCutGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleCPlus.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleD.root" B02DD /home/fmeier/git/b02dd/config/Reducer/FinalSelection.cfg > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_massvetos_bdt_finalselection.log
 
 echo ""
-echo "**Tuple D** (common variables, pre-selection, vetos + PID cuts, BDT selection applied, final selection, FT variables)"
+echo "**Tuple DPlus** (common variables, pre-selection, vetos + PID cuts, BDT selection applied, final selection, FT variables)"
 echo "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleDPlus.root"
 B02DDFlavourTaggingGrimReaper "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleD.root" B02DD "${INPUT_PATH}${BASE_FILE_NAME}_${DATE}_${PRODUCER}_TupleDPlus.root" > scythe_${RUN_NAME}_${DATE}_DT2012_B02DD_vars_preselected_massvetos_bdt_finalselection_ft.log
 
